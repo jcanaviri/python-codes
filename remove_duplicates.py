11d007ee1e6a3f115899a6054ee1f9713c03cfa0
jcanaviri42 <jcanaviri42@gmail.com>
2023-02-28 19:58:50 -0400

    add a new way to remove duplicates from a list

--- remove_duplicates.py
+++ remove_duplicates.py
@@ -9,5 +9,11 @@ def remove_duplicates(lst: List[int]) -> List[int]:
     return cleaned_list
 
 
+def remove_duplicates_v2(lst: List[int]) -> List[int]:
+    return list(set(lst))
+
 without_duplicates = remove_duplicates([1, 2, 3, 2, 1, 4, 5, 4])
+print(without_duplicates)  # Output [1, 2, 3, 4, 5]
+
+without_duplicates = remove_duplicates_v2([1, 2, 3, 2, 1, 4, 5, 4])
 print(without_duplicates)  # Output [1, 2, 3, 4, 5]
